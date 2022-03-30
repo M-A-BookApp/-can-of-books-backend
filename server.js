b7a5714eff47f332a5f584bce2f96f49f6b70dbc
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-03-30 01:45:53 -0700

    delete

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ db.once('open', function () {
 
 const app = express();
 
+//middleware
 app.use(cors());
+
+//must have this to receive json from a request
+app.use(express.json());
 
 const PORT = process.env.PORT || 3002;
 
@@ -25,13 +29,47 @@ app.get('/', (request, response) => {
   response.status(200).send('welcome')
 });
 
-app.get('/books',getBooks);
-async function getBooks(request, response, next){
-  try{
-let results = await Book.find();
-response.status(200).send(results);
+app.get('/books', getBooks);
+app.post('/books', postBooks);
+app.delete('/books/:id', deleteBooks);
+
+//REST=>GET Mongoose Model.find()
+async function getBooks(request, response, next) {
+  try {
+    // let resultsEmail = await Book.find({email:req.query.email})
+    let queryObject = {};
+    if (request.query.location) {
+      queryObject.location = req.query.location;
+    }
+    //gets things from db .find
+    let results = await Book.find();
+    response.status(200).send(results);
   }
-  catch(error){
+  catch (error) {
+    next(error);
+  }
+}
+//REST=>POST Mongoose Model.create()
+async function postBooks(request, response, next) {
+  console.log(request.body);
+  try {
+    let newBookInstance = await Book.create(request.body);
+    response.status(200).send(newBookInstance);
+  }
+  catch (error) {
+    next(error);
+  }
+}
+
+//REST=>Delete Mongoose Model.findByIdDelete()
+async function deleteBooks(request, response, next) {
+  let id = request.params.id;
+  try {
+    console.log(id);
+    await Book.findByIdAndDeleted(id);
+    response.send('Book Deleted');
+  }
+  catch (error) {
     next(error);
   }
 }
